04425eec413d29d784e093f75f3b71a9d0c75737
123nobody <123nobody@gmail.com>
2012-12-14 10:26:22 +0800

    Initial Commit

---
+++ MultipleSelectionTableViewDemo/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  MultipleSelectionTableViewDemo
+//
+//  Created by Wei on 12-12-14.
+//  Copyright (c) 2012年 Wei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ MultipleSelectionTableViewDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MultipleSelectionTableViewDemo
+//
+//  Created by Wei on 12-12-14.
+//  Copyright (c) 2012年 Wei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
